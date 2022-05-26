ed1c09a6fed62d05f2df20209fc7d8a394aac3fe
amacapozzi <amadeocapozzi07@gmail.com>
2022-05-26 05:38:21 +0100

    se agrego conexion a base de datos

--- app.index.js
+++ app.index.js
@@ -2,13 +2,24 @@ const express = require('express');
 const app = express();
 const path = require('path');
 var puerto = '7070'
+const dotenv = require('dotenv')
+const varsession = require('./var.session/va.se');
+const db = require('./mongodb/db.mongodb');
+
+
+
 
 
 app.listen(puerto, ()=>{
     console.log(`Aplicacion lista en http://localhost:${puerto}`)
 });
 
-app.set('view engine', 'ejs', 'html');
+app.set('view engine', 'ejs');
+
+dotenv.config({path:'./env/.env'});
+
+
+
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({extended: false}));
---
+++ var.session/va.se.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+
+const session = require('express-session');
+app.use(session({
+    secret: 'secret',
+    resave:true,
+    saveUninitialized:true
+}));
